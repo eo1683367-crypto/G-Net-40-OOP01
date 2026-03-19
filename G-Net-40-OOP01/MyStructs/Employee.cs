e299fb0c739879704681c7a81488889d74e127e9
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-03-19 16:02:39 +0200

    Question01-Part01

---
+++ G-Net-40-OOP01/MyStructs/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_OOP01.MyStructs
+{
+    internal struct Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public Employee(string name, int age, decimal salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public override string ToString() => $"Employee Name: {Name} , Age: {Age} , Salary = {Salary}";
+    }
+}
--- G-Net-40-OOP01/Program.cs
+++ G-Net-40-OOP01/Program.cs
@@ -1,10 +1,90 @@
-﻿namespace G_Net_40_OOP01
+﻿using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using G_Net_40_OOP01.MyClasses;
+using G_Net_40_OOP01.MyStructs;
+
+namespace G_Net_40_OOP01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            // Part 01 : 
+            // Q1:  Explain with code example how class and struct behave differently
+            // Q2 : Explain the difference between public and private access modifiers with an example.
+            // Q3 : Describe the steps to create and use a class library in Visual Studio.
+            // Q4 : What is a class library? Why do we use class libraries?
+            //------------------------------------------------------------------------------------------
+
+
+            //Answers: Q1:
+            #region Code Example Of Class
+
+            #region First Different : (Value type vs Reference Type)
+            //Student student1 = new Student("Ali", 20);
+
+            //Student student2 = student1; // This creates a reference to the same object in memory
+
+            //Console.WriteLine(student1);
+            //Console.WriteLine(student2);
+
+            //Console.WriteLine("--------------------------------------------------");
+
+            //student2.Name = "Mahmoud"; // Modifying student2 will affect student1
+            //                           // since they reference the same object
+            //                           // Because Classes are reference types,
+            //                           // they are stored on the heap and variables hold references
+            //                           // to the data rather than the data itself.
+
+            //Console.WriteLine(student1);
+            //Console.WriteLine(student2); 
+            #endregion
+            #region Second Different : (Default Constructor)
+            //Student student1 = new Student("Ali", 20); 
+            //// if we define custom constructor in class
+            //// we must initialize all properties in the constructor
+
+
+            //Student student2 = new Student(){Name = "Ali",Age = 20 }; //Invalid
+            ////in class the default constructor is available even if we define custom constructor
+            ////and if you define custom constructor the default constructor is not available 
+            #endregion
+            #endregion
+            #region Code Example Of Struct 
+            #region First Different : (Value type vs Reference Type)
+            Employee emp01 = new Employee("Sara", 30, 5000m);
+
+            Employee emp02 = emp01; // This creates a copy of emp01
+
+            Console.WriteLine(emp01);
+            Console.WriteLine(emp02);
+            Console.WriteLine("-------------------------------------------------------");
+
+            emp02.Salary = 6000m; // Modifying emp02 will NOT affect emp01
+                                  // since they are different instances in memory
+                                  //Because Structs are value types,
+                                  //they are stored on the stack and each variable holds its own copy of the data.
+
+            Console.WriteLine(emp01);
+            Console.WriteLine(emp02);
+            #endregion
+            #region Second Different : (Default Constructor)
+            //Employee emp03 = new Employee("Omar", 25, 4000m); 
+
+            //Employee emp04 = new Employee() { Name ="mohamed", Age = 9,Salary =  1000m};
+
+            //in struct the default constructor is  available if we define custom constructor
+            #endregion
+
+
+            #endregion
+
+
+
+
+            #endregion
         }
     }
 }
